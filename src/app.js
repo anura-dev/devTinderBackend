a85adcb08ec4f80b2e96b6e37434309254f3c5de
Anura Mohaniya <anura.mohaniya@gmail.com>
2025-09-26 18:15:07 +0530

    Multiple Route handlers

--- src/app.js
+++ src/app.js
@@ -25,6 +25,36 @@ app.get("/user/:userid/:name/:password", (req, res) => {
   console.log(req.params);
 });
 
+//route handlers
+
+// e.g:: app.use('/test', [cb1, cb2], cb3, cb4)
+// you can define multiple callback functions that behave like middleware to handle a route.
+// you can also define an array of callback functions to handle a route.
+// you can also mix and match the two.
+// all the callback functions should call next() to pass the control to the next callback function.
+// if a callback function does not call next(), the request will be left hanging.
+// if a callback function sends a response, the request will be left hanging unless next() is called before sending the response.
+// if a callback function sends a response, the subsequent callback functions will not be executed unless next() is called before sending the response.
+// you can also use app.use() to define routes that handle all HTTP methods.
+app.use(
+  "/test",
+  (req, res, next) => {
+    console.log("Inside test middleware");
+    next();
+    res.send("Test middleware");
+  },
+  (req, res, next) => {
+    next();
+    console.log("Inside test middleware 2");
+    // res.send("Test middleware 2");
+  },
+  (req, res, next) => {
+    console.log("Inside test middleware 3");
+    //res.send("Test middleware 3");
+    //next();
+  }
+);
+
 app.listen(port, () => {
   console.log("Server started running on port " + port);
 });
