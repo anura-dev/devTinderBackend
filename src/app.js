d7a957dd731734417f873334091c7dad9c327127
Anura Mohaniya <anura.mohaniya@gmail.com>
2025-09-26 16:09:36 +0530

    ways of routing

--- src/app.js
+++ src/app.js
@@ -5,35 +5,25 @@ const port = 7777;
 //order of routes matter. More specific routes should be defined before less specific ones.
 
 //app.get is used to define routes that specifically handle GET requests.
-app.get("/user", (req, res) => {
-  res.send({ first_name: "John", last_name: "Doe" });
+// /ab+c = /abc, /abbc, /abbbc => b occurs one or more times
+// /ab?c = /ac, /abc ==> b is optional
+// /ab?cd = /acd, /abcd ==> b is optional
+// /ab*c = /abxxxxc => b occurs zero or more times
+// /ab(c|d) = /abc, /abd ==> either c or d
+// /a.b = /a_b, /a-b, /aXb ==> . matches any single character
+// /ab+cd = /abcd, /abbcd, /abbbcd ==> b occurs one or more times
+
+app.get("/abc", (req, res) => {
+  res.send({ first_name: "ANura", last_name: "Doe" });
 });
 
-app.post("/user", (req, res) => {
-  res.send("user route post method");
+//route parameters
+// /user/:userid/:name/:password
+// req.params = { userid: '123', name: 'anura', password: '1234' }
+app.get("/user/:userid/:name/:password", (req, res) => {
+  res.send({ first_name: "ANura", last_name: "Doe" });
+  console.log(req.params);
 });
-
-app.delete("/user", (req, res) => {
-  res.send("user route delete method");
-});
-
-//app.use is used to define middleware and routes in Express.js.
-//It can handle all HTTP methods (GET, POST, PUT, DELETE, etc.) for the specified path.
-
-app.use("/test/2", (req, res) => {
-  res.send("test route 2 use method");
-});
-app.use("/test", (req, res) => {
-  res.send("test route use method");
-});
-
-// app.use("/main", (req, res) => {
-//   res.send("main route");
-// });
-
-// app.use("/", (req, res) => {
-//   res.send("Dashboard route");
-// });
 
 app.listen(port, () => {
   console.log("Server started running on port " + port);
